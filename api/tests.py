cc98d3d1cba4700b5913d71d0cb731d08efd9f31
Parkayun <iamparkayun@gmail.com>
2016-12-27 19:06:42 +0900

    Add ApiView

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,24 @@
+import json
+
 from django.test import TestCase
 
-# Create your tests here.
+from .views import ApiView
+
+
+class ApiViewTestCase(TestCase):
+
+    def test_response(self):
+        test_cases = (
+            (({'hello': 'world'}, 200), {'status': 'success', 'data': {'hello': 'world'}, 'message': None}),
+            (({'hello': 'world'}, 201), {'status': 'success', 'data': {'hello': 'world'}, 'message': None}),
+            (({'hello': 'world'}, 401, 'T^T'), {'status': 'error', 'data': {'hello': 'world'}, 'message': 'T^T'})
+        )
+
+        for case in test_cases:
+            args, expect = case
+            response = ApiView.response(*args)
+            self.assertEqual(json.loads(response.content), expect)
+
+            status_code = args[1] if len(args) >= 2 and isinstance(args[1], int) else 200
+            self.assertEqual(response.status_code, status_code)
+
--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+import typing
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+
+class ApiView(View):
+
+    @staticmethod
+    def response(data: dict={}, status_code: int=200, message: typing.Union[str, None]=None) -> JsonResponse:
+        payload = {
+            'status': 'success' if status_code < 400 else 'error',
+            'data': data,
+            'message': message
+        }
+        response = JsonResponse(payload)
+        response.status_code = status_code
+        return response
+
