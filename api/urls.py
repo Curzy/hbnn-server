5cf0939ff2cd282073059107cb07f76d5c2e8847
leejaedus <leejaedus@users.noreply.github.com>
2017-01-15 19:28:21 +0900

    JWT 인증 추가 (#54)
    close #20 
    
    * Ignore .idea on git
    
    * Ignore additional files
    
    * Ignore db.sqlite3
    
    * `JWTManager` 추가
    
    * `jwt_login_required` 데코레이터 추가
    
    * `CsrfViewMiddleware` 비활성
    
    * `JWTAuthView` 추가
    
    * 테스트 코드 수정
    
    * flake8

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 
-from .views import PingView, UserAPIView
-
+from .views import PingView, UserAPIView, JWTAuthView
 
 urlpatterns = [
     url(r'^ping/$', PingView.as_view(), name='api_ping'),
@@ -9,4 +8,5 @@ urlpatterns = [
     url((r'^users/'
          r'([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$'),
         UserAPIView.as_view(), name='api_user'),
+    url(r'^auth/$', JWTAuthView.as_view(), name='api_auth'),
 ]
